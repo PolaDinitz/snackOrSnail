5f80387b82bb7723cda4a7d76fc03d8538de6f50
DanielBeilin <dbeilin3@gmail.com>
2020-09-06 00:56:25 +0300

    add linkedlist and start of create snakeOrSnail

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.lang.Math;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+
+
+    public static LinkedList CreateSnailOrSnake() {
+        final LinkedList list = new LinkedList();
+        // Snake
+        if(Math.random() < 0.5) {
+            while(Math.random() > 0.01){
+                LinkedList.insert(list, (int) (Math.random() * 100));
+            }
+        } 
+        // Snail 
+        else {
+            LinkedList.Node loopNode = list.head;
+            while(Math.random() > 0.2){
+                if(Math.random() <= 0.015){
+                    
+                }
+            }
+        }
+        return list;
     }
 }
